767ee0736d8ceb91bf747b3d1752a451f43a3a98
Vukoman Stojanovic <vukomans96@yahoo.com>
2022-01-22 22:34:52 +0100

    Update taskList test

--- test/taskList.test.ts
+++ test/taskList.test.ts
@@ -3,14 +3,15 @@ import { formatSwtf } from '../src/index';
 describe('Simple task list', () => {
     it('white spaces task list', () => {
         const rawTasks = `
-        - Create formatter for SWTF [p: high]
-            - Init repository [done]
+        - Create formatter for SWTF [p: high][me]
+
+                - Init repository [done]
             - Create formatter [in_progress]
             - Create README [ready]
         `;
 
         const expectedOutput = [
-            '- Create formatter for SWTF [p: high]\n',
+            '- Create formatter for SWTF [p: high][me]\n',
             `${' '.repeat(4)}- Init repository [done]\n`,
             `${' '.repeat(4)}- Create formatter [in_progress]\n`,
             `${' '.repeat(4)}- Create README [ready]\n`
